ab9d5c42627a64832d70f1a91c629ffb7ba56fff
David <11443294+davidabrowning@users.noreply.github.com>
2024-02-20 15:02:10 -0500

    Added domain to export

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,10 @@
 module group.battleship {
     requires javafx.controls;
-    requires javafx.fxml;
 
 
     opens group.battleship to javafx.fxml;
     exports group.battleship;
+    exports group.battleship.domain;
     exports group.battleship.logic;
     opens group.battleship.logic to javafx.fxml;
     exports group.battleship.ui;
