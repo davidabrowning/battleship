3fa6285cf9d76286eb925a4fe80406c36108d067
David <11443294+davidabrowning@users.noreply.github.com>
2024-02-20 17:06:13 -0500

    Added flag for and support for vertical ship placement

--- src/main/java/group/battleship/domain/Player.java
+++ src/main/java/group/battleship/domain/Player.java
@@ -8,17 +8,27 @@ public class Player {
     private final String name;
     private final Fleet fleet;
     private final List<Integer> shotsSustained;
+    private boolean placeShipHorizontally;
 
     public Player(String name) {
         this.name = name;
         this.fleet = new Fleet();
         this.shotsSustained = new ArrayList<>();
+        this.placeShipHorizontally = true;
     }
 
     public String getName() { return name; }
     public Fleet getFleet() { return fleet; }
     public List<Integer> getShotsSustained() { return shotsSustained; }
     public void sustainShot(int tileNum) { shotsSustained.add(tileNum); }
+    public boolean placeShipHorizontally() { return placeShipHorizontally; }
+    public void rotateShipPlacement() {
+        if (placeShipHorizontally) {
+            placeShipHorizontally = false;
+        } else {
+            placeShipHorizontally = true;
+        }
+    }
     @Override
     public String toString() { return getName(); }
 }
