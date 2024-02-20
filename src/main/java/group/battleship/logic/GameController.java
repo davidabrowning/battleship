1b54ff05b799fd7beabd659dcc85bd9bdeb9f825
David <11443294+davidabrowning@users.noreply.github.com>
2024-02-20 17:05:57 -0500

    Added flag for and support for vertical ship placement

--- src/main/java/group/battleship/logic/GameController.java
+++ src/main/java/group/battleship/logic/GameController.java
@@ -43,14 +43,24 @@ public class GameController {
     public boolean isValidShipPlacementLocation(int tileNum, int shipLength, Fleet alreadyPlacedFleet) {
         int row = tileNum / 10;
         int col = tileNum % 10;
+        boolean horizontalOrientation = game.getActivePlayer().placeShipHorizontally();
 
-        if (col + shipLength > 10) {
+        if (horizontalOrientation && col + shipLength > 10) {
+            return false;
+        }
+        if (!horizontalOrientation && row + shipLength > 10) {
             return false;
         }
 
         int[] desiredTiles = new int[shipLength];
-        for (int i = 0; i < shipLength; i++) {
-            desiredTiles[i] = tileNum + i;
+        if (horizontalOrientation) {
+            for (int i = 0; i < shipLength; i++) {
+                desiredTiles[i] = tileNum + i;
+            }
+        } else {
+            for (int i = 0; i < shipLength; i++) {
+                desiredTiles[i] = tileNum + i * 10;
+            }
         }
         for (int desiredTile : desiredTiles) {
             if (alreadyPlacedFleet.containsLocation(desiredTile)) {
@@ -62,8 +72,14 @@ public class GameController {
     }
 
     public void placeShip(Ship ship, int tileNum) {
-        for (int i = 0; i < ship.getSize(); i++) {
-            ship.addLocation(tileNum + i);
+        if (game.getActivePlayer().placeShipHorizontally()) {
+            for (int i = 0; i < ship.getSize(); i++) {
+                ship.addLocation(tileNum + i);
+            }
+        } else {
+            for (int i = 0; i < ship.getSize(); i++) {
+                ship.addLocation(tileNum + i * 10);
+            }
         }
     }
 
@@ -72,7 +88,7 @@ public class GameController {
     }
 
     public boolean allShipsArePlaced(Player p) {
-        return p.getFleet().hasBeenPlaced();
+        return !p.getFleet().hasNotBeenPlaced();
     }
 
     public boolean allShipsArePlaced() {
@@ -108,5 +124,9 @@ public class GameController {
     public boolean isGameOver() {
         return game.isGameOver();
     }
+
+    public boolean placeShipHorizontally(Player p) { return p.placeShipHorizontally(); }
+
+    public void rotateShipPlacement(Player p) { p.rotateShipPlacement(); }
 
 }
